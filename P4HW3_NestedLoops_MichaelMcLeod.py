2498a48691d3642d3ae195ab04ae82af1647f744
michaelmcleod98 <60890942+michaelmcleod98@users.noreply.github.com>
2020-03-09 12:10:26 -0400

    Add files via upload

---
+++ P4HW3_NestedLoops_MichaelMcLeod.py
@@ -0,0 +1,17 @@
+# nesting loops
+#3/9/20
+#CTI-110 P4HW3 - Nested Loops
+#Michael McLeod
+def main():
+
+    NUM_STEPS = 6
+
+    for r in range (NUM_STEPS):
+        print('#', end='')
+        for c in range (r):
+            print(' ', end='')
+        print('#')
+
+
+
+main()
