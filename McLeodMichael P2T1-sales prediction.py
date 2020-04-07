911052cb9b5d2c805f01f87258ff1a2ada45a11e
michaelmcleod98 <60890942+michaelmcleod98@users.noreply.github.com>
2020-04-07 17:33:48 -0400

    Add files via upload

---
+++ McLeodMichael P2T1-sales prediction.py
@@ -0,0 +1,13 @@
+#calculating the profict of a companies sales prediction
+#4/7/20
+#CTI-110 P2T1 - Sales Prediction
+#Michael McLeod
+#
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Diusplay profit.
+print('profit is $', format(profit, ',.2f'))
